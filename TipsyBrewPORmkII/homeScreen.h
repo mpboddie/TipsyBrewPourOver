a6cc4e6265c5cc5aff9b94a1bf339981fbb02daf
Mark Boddie <mpboddie@gmail.com>
2021-02-13 22:37:35 -0500

    Changed a bit of the button drawing to add the ability to use big buttons as a toggle. See the Pre-heat button as an example. Other minor UI additions as well.

--- TipsyBrewPORmkII/homeScreen.h
+++ TipsyBrewPORmkII/homeScreen.h
@@ -1,19 +1,42 @@
-int homeMenuPos = 0;
+bool preHeatStatus = false;
 
+void drawPreHeatButton() {
+  if (preHeatStatus) {
+    drawBigButton(LEFT_ON, BOTTOM, flame, TB_ORANGE, MOSTLY_WHITE, "Pre-heat", MOSTLY_WHITE, TB_ORANGE, "ON");
+  } else {
+    drawBigButton(LEFT_OFF, BOTTOM, flame, TB_ORANGE, MOSTLY_WHITE, "Pre-heat", MOSTLY_WHITE, TB_ORANGE, "OFF");
+  }
+}
+
+void togglePreHeat() {
+  preHeatStatus = !preHeatStatus;
+  drawPreHeatButton();
+  if (preHeatStatus) {
+    // turn on the heat
+    // set a timer to turn it off, just in case the user turns on preheat and walks away we don't want all the water to evaporate and dry boil
+  } else {
+    // turn off heat and cancel a timer if it were on
+  }
+}
 
 void switchToHome() {
   appMode = APP_HOME;
-
+  topBarHomeScreen();
+  
   brewerStateLED(READY_NOW);
   
   // clear the area under the top bar
   tft.fillRect(0, 26, tft.width(), tft.height()-26, BKGD);
 
-  drawBigButton(LEFT_COL, TOP, cardList, TB_ORANGE, MOSTLY_WHITE, "Logs", MOSTLY_WHITE, GREY_GRAY, "Work in progress");
+  drawBigButton(LEFT_OFF, TOP, cardList, TB_ORANGE, MOSTLY_WHITE, "Logs", MOSTLY_WHITE, GREY_GRAY, "COMING SOON");
+
+  drawPreHeatButton();
   
-  drawBigButton(LEFT_COL, BOTTOM, flame, TB_ORANGE, MOSTLY_WHITE, "Pre-heat", MOSTLY_WHITE, TB_ORANGE);
+  drawBigButton(RIGHT_OFF, TOP, coffeeBean, MOSTLY_WHITE, TB_ORANGE, "Coffee", TB_ORANGE, MOSTLY_WHITE);
 
-  drawBigButton(RIGHT_COL, TOP, coffeeBean, MOSTLY_WHITE, TB_ORANGE, "Coffee", TB_ORANGE, MOSTLY_WHITE);
+  drawBigButton(RIGHT_OFF, BOTTOM, gear, MOSTLY_WHITE, TFT_BLUE, "Settings", TFT_BLUE, MOSTLY_WHITE);
+}
 
-  drawBigButton(RIGHT_COL, BOTTOM, gear, MOSTLY_WHITE, TFT_BLUE, "Settings", MOSTLY_WHITE, TFT_BLUE);
+void homeLeftLower() {
+  togglePreHeat();
 }
