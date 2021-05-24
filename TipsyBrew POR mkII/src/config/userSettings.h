b7f4f45b3c08d9edc3b126ec49786ad7451dd929
Mark Boddie <mpboddie@gmail.com>
2021-05-24 00:08:50 -0400

    settings changes

--- TipsyBrew POR mkII/src/config/userSettings.h
+++ TipsyBrew POR mkII/src/config/userSettings.h
@@ -3,11 +3,17 @@
 
 // WiFi settings
 // These must be customized for your network. It should be self explanitory.
-const char *ssid     = "EyesOnBack";
-const char *password = "Il!kech33z!ts";
-#define WIFI_TIMEOUT            20000
-#define WIFI_RECOVER_TIME       20000
+#define WIFI_NETWORK            "EyesOnBack"
+#define WIFI_PASSWORD           "Il!kech33z!ts"
+#define WIFI_TIMEOUT_MS         20000 // 20 second WiFi connection timeout
+#define WIFI_RECOVER_TIME_MS    30000 // Wait 30 seconds after a failed connection attempt
 
-const char *hostname = "TipsyBrew";
+#define HOSTNAME                "TipsyBrew"
+
+// CUSTOM COLORS
+#define TB_ORANGE       0xFA20    // 255, 68, 0
+#define MOSTLY_WHITE    0xAD75    // 175, 175, 175
+#define BKGD            0x0000    // 0, 0, 0
+#define GREY_GRAY       0x8410    // 128, 128, 128
 
 #endif
