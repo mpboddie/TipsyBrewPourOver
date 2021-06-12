5aae1d352edee186c71ecbfa3c001a2e17e0300a
Mark Boddie <mpboddie@gmail.com>
2021-06-11 23:49:21 -0400

    Added some new icons

--- TipsyBrew POR mkII/src/tasks/display.h
+++ TipsyBrew POR mkII/src/tasks/display.h
@@ -191,16 +191,20 @@ void updateDisplay(void * parameter){
                     tft.setTextDatum(TC_DATUM);
                     tft.drawString("This is coming soon", tft.width()/2, 60, 4);
                     tft.drawString("Tap to return Home for now", tft.width()/2, tft.fontHeight(4)+70, 4);*/
-                    drawLittleButton(LEFT_OFF, TOP, arrowUp, TFT_BLUE, MOSTLY_WHITE);
-                    drawLittleButton(LEFT_OFF, BOTTOM, homeSmall, TFT_BLACK, MOSTLY_WHITE);
-                    drawLittleButton(RIGHT_OFF, TOP, arrowUp, MOSTLY_WHITE, TFT_BLUE);
-                    drawLittleButton(RIGHT_OFF, BOTTOM, arrowDown, MOSTLY_WHITE, TB_ORANGE);
-                    tft.setViewport(tft.width()*(3/8), 26, tft.width()*(5/8)-55, tft.height()-50);
-                    tft.drawString("PREPARE", 0, 0, 4);
-                    tft.drawString("1. Put Carafe in TipsyBrew", tft.width()*(3/8), 5+tft.fontHeight(4), 2);
-                    tft.drawString("2. Put EMPTY Cone in TipsyBrew", tft.width()*(3/8), (2*5)+tft.fontHeight(2)+tft.fontHeight(4), 2);
-                    tft.drawString("3. Put Filter in Cone", tft.width()*(3/8), (3*5)+(2*tft.fontHeight(2))+tft.fontHeight(4), 2);
-                    tft.resetViewport();
+                    
+                    tft.setTextColor(MOSTLY_WHITE, BKGD);
+                    tft.setTextDatum(TL_DATUM);
+                    tft.drawString("PREPARE", 55, 65, 4);
+                    tft.drawString("1. Put Carafe in TipsyBrew", 55, 65+5+tft.fontHeight(4), 2);
+                    tft.drawString("2. Put EMPTY Cone in TipsyBrew", 55, 65+(2*5)+tft.fontHeight(2)+tft.fontHeight(4), 2);
+                    tft.drawString("3. Put Filter in Cone", 55, 65+(3*5)+(2*tft.fontHeight(2))+tft.fontHeight(4), 2);
+                    tft.setTextDatum(TR_DATUM);
+                    tft.drawString("CONTINUE ->", tft.width()-55, tft.height()-65, 2);
+                    
+                    drawLittleButton(LEFT_OFF, TOP, rain, TFT_BLUE, MOSTLY_WHITE);
+                    drawLittleButton(LEFT_OFF, BOTTOM, home, TFT_BLACK, MOSTLY_WHITE);
+                    drawLittleButton(RIGHT_OFF, TOP, cold, MOSTLY_WHITE, TFT_BLUE);
+                    drawLittleButton(RIGHT_OFF, BOTTOM, arrowRight, MOSTLY_WHITE, TB_ORANGE);
                 }
 
                 if (tft.getTouch(&x, &y))
